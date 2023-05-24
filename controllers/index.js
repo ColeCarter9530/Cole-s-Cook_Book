714f3025484f2e995cad2e4dceb12fbed908f02d
ColeC <contactcolecarter@gmail.com>
2023-05-24 18:42:22 -0400

    created a post request to /users/sign-up. it works :)

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
+const users = require('./users');
 // localhost:3001
-router.get('/', async (req, res) => {
-    res.json('HI!');
-});
+// router.get('/', async (req, res) => {
+//     res.json('HI!');
+// });
+
+// localhost:3001/users
+router.use('/users', users);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const controllers = require('./controllers');
 const PORT = 3001;
 const app = express();
-// const models = require('./models')
+
+app.use(express.json());
+app.use(express.urlencoded( { extended: true}));
+const models = require('./models')
 const sequelize = require('./config/connection');
 
 app.use(controllers);
