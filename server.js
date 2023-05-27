74737435ccaf851319a58112c292e7b272827c79
ColeC <contactcolecarter@gmail.com>
2023-05-26 21:31:45 -0400

    changed port number from static to flexible after heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const controllers = require('./controllers');
 const exphbs = require('express-handlebars');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // access to the public folder
