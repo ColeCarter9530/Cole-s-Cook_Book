8b6e6ae8cb29f039290ed83471467196a74e561a
ColeC <contactcolecarter@gmail.com>
2023-05-27 18:12:04 -0400

    created sumbit button

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,22 @@
 console.log('hello');
+
+const recipeForm = document.getElementById('recipeForm')
+
+const formSubmission = (e) => {
+    e.preventDefault();
+
+    const firstNameValue = document.getElementById('firstNameInput').value;
+    console.log(firstNameValue);
+    const lastNameValue = document.getElementById('lastNameInput').value;
+    console.log(lastNameValue);
+    const dishNameValue = document.getElementById('dishNameInput').value;
+    console.log(dishNameValue);
+    const ingredientsValue = document.getElementById('ingredientsInput').value;
+    console.log(ingredientsValue);
+    const instructionsValue = document.getElementById('instructionsInput').value;
+    console.log(instructionsValue);
+    const emailValue = document.getElementById('emailInput').value;
+    console.log(emailValue);
+}
+
+recipeForm.addEventListener('submit', formSubmission)
